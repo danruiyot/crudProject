089985abbb3545f4e022990c2521e32a66b036d6
dan ruiyot <danruiyot@gmail.com>
2019-07-28 18:29:16 +0300

    renamed:    database/migrations/2019_07_23_163740_create_employees_table.php -> database/migrations/2019_07_28_152304_create_employees_table.php

--- database/migrations/2019_07_28_152304_create_employees_table.php
+++ database/migrations/2019_07_28_152304_create_employees_table.php
@@ -37,3 +37,4 @@ class CreateEmployeesTable extends Migration
     }
 }
 
+
---
+++ database/migrations/employees.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+	    $table->text('fname');
+	    $table->text('lname');
+	    $table->text('email');
+	    $table->integer('company_id')->unsigned();
+	    $table->timestamp('created_at');
+	    $table->timestamp('updated_at');
+	    $table->foreign('company_id')->references('id')->on('companies');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
+
+
